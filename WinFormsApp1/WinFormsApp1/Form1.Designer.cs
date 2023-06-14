df026fd104f658c83b79ca3ebbd5fa359cb387be
reiji <reiji128@icloud.com>
2023-06-14 09:33:59 +0900

    ファイルとヘルプのタブの作成

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            menuFile = new ToolStripMenuItem();
+            menuHelp = new ToolStripMenuItem();
+            menuFileOpen = new ToolStripMenuItem();
+            menuFIileExit = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuFile, menuHelp });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            menuFile.DropDownItems.AddRange(new ToolStripItem[] { menuFileOpen, menuFIileExit });
+            menuFile.Name = "menuFile";
+            menuFile.Size = new Size(67, 20);
+            menuFile.Text = "ファイル(&F)";
+            // 
+            // menuHelp
+            // 
+            menuHelp.Name = "menuHelp";
+            menuHelp.Size = new Size(65, 20);
+            menuHelp.Text = "ヘルプ(&H)";
+            // 
+            // menuFileOpen
+            // 
+            menuFileOpen.Name = "menuFileOpen";
+            menuFileOpen.Size = new Size(180, 22);
+            menuFileOpen.Text = "開く(&O)";
+            // 
+            // menuFIileExit
+            // 
+            menuFIileExit.Name = "menuFIileExit";
+            menuFIileExit.Size = new Size(180, 22);
+            menuFIileExit.Text = "終了(&X)";
+            menuFIileExit.Click += menuFIileExit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuFile;
+        private ToolStripMenuItem menuHelp;
+        private ToolStripMenuItem menuFileOpen;
+        private ToolStripMenuItem menuFIileExit;
+    }
+}
